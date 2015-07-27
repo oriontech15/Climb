7f2bf3abf5d57900d21337b8bcdafac379da297d
Justin Smith <11jsmith7@gmail.com>
2015-07-27 16:10:29 -0600

    Initial Commit

---
+++ framedUP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  framedUP
+//
+//  Created by Justin Smith on 7/27/15.
+//  Copyright (c) 2015 Justin Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ framedUP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  framedUP
+//
+//  Created by Justin Smith on 7/27/15.
+//  Copyright (c) 2015 Justin Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
